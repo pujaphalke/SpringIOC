8c40a6338012f1e49fc2f4a789f25c8d60e27e6a
pujaphalke <pujaphalke1997@gmail.com>
2025-09-29 16:16:38 +0530

    Update MyfirstSpringbootapplicationApplication.java
    Added SOP statement in main class.

--- myfirstSpringbootapplication/src/main/java/com/springboot/myfirstSpringbootapplication/MyfirstSpringbootapplicationApplication.java
+++ myfirstSpringbootapplication/src/main/java/com/springboot/myfirstSpringbootapplication/MyfirstSpringbootapplicationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyfirstSpringbootapplicationApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Pull from GitHub");
 		SpringApplication.run(MyfirstSpringbootapplicationApplication.class, args);
 	}
 
